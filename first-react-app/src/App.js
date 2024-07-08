48682a7f27148e28eadf85c6a95e4181ef6cc794
SreelekhaMysolla <sreelekhamysolla@gmail.com>
2024-07-08 14:13:16 +0530

    Added Layouts

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,73 +1,5 @@
-// -----------------------Components , State , Hooks and Props -----------------------------------
-/**
-import "./App.css";
-
-// Importing call component
-import ClassComponent from "./Components/ClassComponent";
-import FunctionComponent from "./Components/FunctionComponent";
-import FunctionComponent1 from "./Components/PropsFunctionComponent";
-import FunctionComponent2 from "./Components/UpdatingData";
-import FunctionComponent3 from "./Components/UpdateUsingButton";
-import FunctionComponent4 from "./Components/UpdateUsingInput";
-
-// ---- Updating a value ----
-import { useState } from "react";
-
-function App() {
-  const [name, setName] = useState("Ravali Katkam"); // --- updating a value ----
-  const [names, setNames] = useState("Ravali Katkam");
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p className="App-P"> Class Components </p>
-        <ClassComponent />
-        <br />
-
-        <p className="App-P"> Function Components </p>
-        <FunctionComponent />
-        <br />
-
-        <p className="App-P"> Using Props </p>
-        <FunctionComponent1
-          name="Ravali"
-          age={20}
-          graduation="St Francis College for women"
-        />
-
-        <p className="App-P">
-          Updating the name data internally and adding course data externally
-        </p>
-        <FunctionComponent2
-          name={name}
-          age={20}
-          graduation="St Francis College for women"
-        />
-
-        <p className="App-P"> Updating the data using button </p>
-        <FunctionComponent3
-          name={name}
-          age={20}
-          graduation="St Francis College for women"
-          setName={setName}
-        />
-
-        <p className="App-P"> Updating the data using input </p>
-        <FunctionComponent4
-          name={names}
-          age={20}
-          graduation="St Francis College for women"
-          setName={setNames}
-        />
-      </header>
-    </div>
-  );
-}
-
-export default App;
- */
-
 // ---------------------------- Routes -------------------------------
-/**
+
 import "./App.css";
 
 import ClassComponent from "./Components/ClassComponent";
@@ -83,6 +15,7 @@ import { useState } from "react";
 
 function App() {
   const [name, setName] = useState("Ravali Katkam"); // --- updating a value ----
+  const [nameValue, setNameValue] = useState("Ravali Katkam");
   const [names, setNames] = useState("Ravali Katkam");
   return (
     <div className="App">
@@ -114,10 +47,10 @@ function App() {
             path="/updateUsingButton"
             element={
               <FunctionComponent3
-                name={name}
+                name={nameValue}
                 age={20}
                 graduation="St Francis College for women"
-                setName={setName}
+                setName={setNameValue}
               />
             }
           />
@@ -135,55 +68,6 @@ function App() {
         </Routes>
       </header>
     </div>
-  );
-}
-
-export default App;
-*/
-
-// ------------------------------------- Layouts --------------------------------------------
-import "./App.css";
-
-import ClassComponent from "./Pages/ClassComponent";
-import FunctionComponent from "./Pages/FunctionComponent";
-
-import { Routes, Route } from "react-router-dom";
-
-// ---- Updating a value ----
-import { useState } from "react";
-
-// ------- Basic Layout --------
-import BaseLayoutPage from "./Layout/BaseLayout";
-
-function App() {
-  const course = "Bsc Honors";
-  const [name, setName] = useState("Ravali Katkam");
-  return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <BaseLayoutPage>
-            <ClassComponent />
-          </BaseLayoutPage>
-        }
-      />
-
-      <Route
-        path="/function-component"
-        element={
-          <BaseLayoutPage>
-            <FunctionComponent
-              name={name}
-              age={20}
-              graduation="St Francis College for women"
-              course={course}
-              setName={setName}
-            />
-          </BaseLayoutPage>
-        }
-      />
-    </Routes>
   );
 }
 
